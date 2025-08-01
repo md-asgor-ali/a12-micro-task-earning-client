cfdb9c0560bb16adaee499935a2612abb683125f
md-asgor-ali <asgor542@gmail.com>
2025-08-01 21:20:42 +0600

    solve a silly mistake

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -85,7 +85,7 @@ const onSubmit = async (data) => {
         name: user.displayName,
         email: user.email,
         photoURL: user.photoURL,
-        role: "user", // default role
+        role: "Buyer", // default role
       };
 
       // Try to create user in DB
